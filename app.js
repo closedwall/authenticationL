839d654f3ff3ddeee935667cf5573fc05b695523
closedwall <yadavrajesh5612@gmail.com>
2023-10-07 14:59:58 +0530

    complete level 5 authentication with google (OAuth)

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import mongoose from "mongoose";
 import session from 'express-session';
 import passport from 'passport';
 import passportLocalMongoose from "passport-local-mongoose";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import findOrCreate  from 'mongoose-findorcreate';
 
 
 const saltRounds = 10;
@@ -26,22 +28,62 @@ app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
+
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
 
+
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        // const profile_id = profile.id;
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/",(req,res)=>{
     res.render("home.ejs")
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+  );
+
+  app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 app.get("/register",(req,res)=>{
     res.render("register.ejs");
 })
